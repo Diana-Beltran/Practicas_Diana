4290d1946bfdda1674e0cad660b7e8349404b539
Diana-Beltran <beltran.diana66@uabc.edu.mx>
2025-05-24 14:39:33 -0700

    Practica # 10
    Añadí los código de las graficas en el mismo archivo  de la practica 9.

--- codigo_titanic.py
+++ codigo_titanic.py
@@ -1,4 +1,6 @@
 import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
 titanic_df = pd.read_csv('titanic.csv')
 
 """ Dimensiones del DataFrame """
@@ -12,24 +14,71 @@ titanic_df = pd.read_csv('titanic.csv')
 #print(titanic_df.head(10))
 """Las ultimas 10 columnas"""
 #print(titanic_df.tail(10))
+""" Informacion del pasajero 148 """
 pasajero_148= titanic_df[titanic_df['PassengerId']== 148]
 #print(pasajero_148)
+""" Las filas pares """
 filas_pares = titanic_df[titanic_df.index % 2 == 0 ]
 #print(filas_pares)
+
+
+""" Los nombres de las personas que iban en primera clase """
 nombres_primera_clase = titanic_df[titanic_df['Pclass'] ==1] ['Name'].sort_values()
 #print(nombres_primera_clase)
-#print(titanic_df.columns)
 
+
+#%%
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib
+
+titanic_df = pd.read_csv('titanic.csv')
+
+""" Mostrar por pantalla el porcentaje de personas que sobrevivieron y murieron"""
 total_pasajeros = titanic_df.shape[0]
 sobrevivientes = (titanic_df['Survived'] == 1).sum() / total_pasajeros * 100
 no_sobrevientes = 100 - sobrevivientes
 #print(f"El porcentaje de sobrevivientes es: {sobrevivientes:.2f}%")
 #print(f"El porcentaje de no sobrevivientes es: {no_sobrevientes:.2f}%")
-#print(titanic_df.Age)
+
+""" CODIGO DE LA GRAFICA DE BARRAS """
+
+estado = ['Sobrevivientes', 'No sobrevivientes']
+porcentajes = [sobrevivientes, no_sobrevientes]
+
+plt.figure(figsize=(12,6))
+plt.bar(estado, porcentajes ,width=0.4, color=['pink','brown'])
+plt.title("Porcentaje de Sobrevivientes y No Sobrevivientes")
+plt.xlabel('Estado')
+plt.ylabel('Porcentaje (%)')
+plt.ylim(0,100)
+plt.show()
+#%%
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+titanic_df = pd.read_csv('titanic.csv')
+""" Mostrar por pantalla el porcenjate de personas que sobrevivieron en cada clase """
 porcentaje_por_clase = titanic_df.groupby('Pclass')['Survived'].mean() * 100
 #print(porcentaje_por_clase)
-#print(titanic_df.Age)
 
+""" CODIGO DE LA GRAFICA"""
+
+sns.barplot(x=porcentaje_por_clase.index, y=porcentaje_por_clase.values, palette='pastel')
+plt.title("Porcentaje de Sobrevivientes por Clase")
+plt.xlabel("Clase")
+plt.ylabel("Porcentaje de Sobrevivientes (%)")
+plt.ylim(0, 100)
+plt.show()
+#%%
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+titanic_df = pd.read_csv('titanic.csv')
+
+""" Mostrar por pantalla la edad media de las mujeres que viajaban en cada clase"""
 titanic_df = titanic_df.dropna(subset=['Age'])
 #print(titanic_df.Age)
 
@@ -40,12 +89,39 @@ edad_media_mujeres_por_clase = mujeres.groupby('Pclass')['Age'].mean()
 #print("La edad media de las mujeres por claes es:" )
 #print(edad_media_mujeres_por_clase)
 
+
+""" CODIGO DE LA GRAFICA"""
+
+sns.barplot(x=edad_media_mujeres_por_clase.index, y=edad_media_mujeres_por_clase.values, palette='pastel')
+plt.title("Edad Media de Mujeres por Clase")
+plt.xlabel("Clase")
+plt.ylabel("Edad Media")
+plt.ylim(0, titanic_df['Age'].max()) 
+plt.show()
+
+#%%
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+titanic_df = pd.read_csv('titanic.csv')
 titanic_df['MenorEdad'] = titanic_df['Age'] < 18 
 #print(titanic_df.MenorEdad)
 
 
-
+""" Mostrar por pantalla el porcentaje de menores y mayores de edad que
+sobrevivieron en cada clase."""
 # Agrupamos por clase y condición de edad, y calculamos porcentaje de sobrevivencia
 porcentaje = titanic_df.groupby(['Pclass', 'MenorEdad'])['Survived'].mean() * 100
-print(porcentaje)
+#print(porcentaje)
 
+""" CODIGO DE LA GRAFICA """
+
+# Convertimos 'porcentaje' a DataFrame para que Seaborn pueda graficar
+porcentaje = porcentaje.reset_index()
+sns.barplot(data=porcentaje, x='Pclass', y='Survived', hue='MenorEdad', palette='pastel')
+plt.title("Porcentaje de Sobrevivientes por Clase y Edad")
+plt.xlabel("Clase")
+plt.ylabel("Porcentaje de Sobrevivientes (%)")
+plt.ylim(0, 100)
+plt.legend(title="¿Menor de edad?")
+plt.show()
